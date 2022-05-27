78136ed4bfeeac709ab7c2a65b918a65a0311210
teruteru-bose <takemoto9570@gmail.com>
2022-05-27 15:41:02 +0900

    非ログイン時、ページ遷移修正

--- app/controllers/prototypes_controller.rb
+++ app/controllers/prototypes_controller.rb
@@ -1,6 +1,6 @@
 class PrototypesController < ApplicationController
   before_action :set_prototype, only: [:show, :edit, :update, :destroy]
-  before_action :authenticate_user!, expect: [:index, :show]
+  before_action :authenticate_user!, except: [:index, :show]
   before_action :move_to_index, except: [:index, :show, :new, :create]
 
   def index
